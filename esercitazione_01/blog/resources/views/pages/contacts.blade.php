d71cbbf7ee2c2a8f4554423e5a17f658559baaf3
Giuseppe Barraco <beppechef.gb@gmail.com>
2024-03-21 20:20:09 +0100

    Quinta esercitazione

--- esercitazione_01/blog/resources/views/pages/contacts.blade.php
+++ esercitazione_01/blog/resources/views/pages/contacts.blade.php
@@ -2,4 +2,25 @@
     <div class="titolo">
         <h1>Contatti</h1>
     </div>
+
+    <x-successo/>
+
+    <x-errore/>
+
+    <form action="{{ route('contatti.send') }}" method="POST">
+                @csrf
+                <div class="row g-3">
+                    <div class="col-12">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email" class="form-control">
+                    </div>
+                    <div class="col-12">
+                        <label for="message">Messaggio</label>
+                        <textarea name="message" id="message" rows="5" class="form-control"></textarea>
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary">Invia</button>
+                    </div>
+                </div>
+            </form>
 </x-layout>
--- esercitazione_01/blog/routes/web.php
+++ esercitazione_01/blog/routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'welcome'])->name('homepage');
 
-Route::get('/contatti', [PageController::class, 'contacts'])->name('contatti');
+Route::get('/contatti', [ContactController::class, 'viewForm'])->name('contatti');
+Route::post('/contatti/send', [ContactController::class, 'send'])->name('contatti.send');
 
 Route::get('/chi-sono', [PageController::class, 'chi_sono'])->name('chi-sono');
 
